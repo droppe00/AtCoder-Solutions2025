d3fa04f93b62a4e592794e273e3bb61c74fbfdf6
kouzouyaruo <kouzouyaruo@gmail.com>
2025-11-11 00:43:56 +0900

    あ

---
+++ ABC350B/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace AtCoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int count = input.Count(c => c == '1');
+
+            Console.WriteLine(count);
+        }
+    }
+}

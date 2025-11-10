522ed7190808271a7844bf12c76413d8f4f84db2
droppe00 <droppe00@gmail.com>
2025-11-11 00:47:54 +0900

    アカウント変更

--- ABC350B/Program.cs
+++ ABC350B/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            // 単純に三マスが0,1だったら足し算する、しないの話
             string input = Console.ReadLine();
 
             int count = input.Count(c => c == '1');

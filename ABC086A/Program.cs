632acb22c48c82f5ecea538895d6f2ac1e441604
droppe00 <droppe00@gmail.com>
2025-11-11 23:16:58 +0900

    ABC086A - Product

---
+++ ABC086A/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+    {
+    static void Main(string[] args)
+    {
+        // 整数aとbの読み込み(1行目：スペース区切り)
+        string[] ab = Console.ReadLine().Split();
+        int a = int.Parse(ab[0]);
+        int b = int.Parse(ab[1]);
+
+        // 計算
+        int product = a * b;
+
+        // 計算結果を2で割って、偶数・奇数のチェック
+        if (product % 2 == 0)
+        {
+            Console.WriteLine("Even");
+        }
+        else
+        {
+            Console.WriteLine("Odd");
+        }
+    }
+    }

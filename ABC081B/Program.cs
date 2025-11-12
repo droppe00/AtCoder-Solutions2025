c9fd64b6102ccd589e1462786834295993d96f4d
droppe00 <droppe00@gmail.com>
2025-11-12 23:49:07 +0900

    ABC081B

---
+++ ABC081B/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        // 各数を2で割れる回数の最小値を求める。
+        int minOperations = a.Select(CountDivisibleByTwo).Min();
+
+        Console.WriteLine(minOperations);
+    }
+
+    static int CountDivisibleByTwo(int x)
+    {
+        int count = 0;
+        while (x % 2 == 0)
+        {
+            x /= 2;
+            count++;
+        }
+
+        return count;
+    }
+}
